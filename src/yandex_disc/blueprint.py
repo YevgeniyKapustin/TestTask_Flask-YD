052bc8ad7460197bd5990bf1748508621351e778
Yevgeniy Kap <kapustaevg@gmail.com>
2024-09-04 11:49:22 +0300

    add download folder

--- src/yandex_disc/blueprint.py
+++ src/yandex_disc/blueprint.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request
 
-from src.yandex_disc.services import fetch_files_info, get_download_link
+from src.yandex_disc.services import fetch_files_info, download_file
 
 blueprint = Blueprint('yandex_disc', __name__, template_folder='templates')
 
@@ -8,11 +8,13 @@ blueprint = Blueprint('yandex_disc', __name__, template_folder='templates')
 @blueprint.route('/', methods=['GET', 'POST'])
 async def index():
     files = None
+    public_key = None
     if request.method == 'POST':
         public_key = request.form['public_key']
         files = await fetch_files_info(public_key)
     return render_template(
         'yandex_disc/index.html',
         files=files,
-        get_download_link=get_download_link
+        download_file=download_file,
+        folder_public_key=public_key if files and len(files) > 1 else None
     )
