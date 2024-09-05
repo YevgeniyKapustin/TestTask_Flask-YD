212a070cbe7970838fe38ae680343a6773dd2984
Yevgeniy Kap <kapustaevg@gmail.com>
2024-09-05 18:28:22 +0300

    add typing

--- src/yandex_disc/blueprint.py
+++ src/yandex_disc/blueprint.py
@@ -7,18 +7,21 @@ blueprint = Blueprint('yandex_disc', __name__, template_folder='templates')
 
 @blueprint.route('/', methods=['GET', 'POST'])
 def index():
-    files = None
-    public_key = None
-    selected_type = None
-    filtered_files = None
+    """Отображает основную страницу для работы с яндекс диском."""
+    files: list[dict] | None = None
+    filtered_files: list[dict] | None = None
+    public_key: str | None = None
+    selected_type: str | None = None
+
     if request.method == 'POST':
         session['form_data'] = request.form
-        public_key = request.form.get('public_key') or session.get('public_key')
-        selected_type = request.form.get('selected_type') or session.get('selected_type')
+        public_key: str | None = request.form.get('public_key') or session.get('public_key')
+        selected_type: str | None = request.form.get('selected_type') or session.get('selected_type')
         if public_key:
-            files = fetch_files_info(public_key)
+            files: list[dict] | None = fetch_files_info(public_key)
         if selected_type and selected_type != 'all':
-            filtered_files = [file for file in files if file.get('mime_type') == selected_type]
+            filtered_files: list[dict] | None = [file for file in files if file.get('mime_type') == selected_type]
+
     return render_template(
         'yandex_disc/index.html',
         files=filtered_files if filtered_files else files,
--- src/yandex_disc/services.py
+++ src/yandex_disc/services.py
@@ -1,17 +1,18 @@
 import requests
 from cachetools import TTLCache, cached
-
+from requests import Response
 
 cache = TTLCache(maxsize=100, ttl=30)
 
 
 @cached(cache)
-def fetch_files_info(public_key):
-    url = f'https://cloud-api.yandex.net/v1/disk/public/resources?public_key={public_key}'
+def fetch_files_info(public_key) -> list[dict]:
+    """Возвращает информацию о файлах содержащихся по переданному public_key."""
+    url: str = f'https://cloud-api.yandex.net/v1/disk/public/resources?public_key={public_key}'
 
-    response = requests.get(url)
+    response: Response = requests.get(url)
     if response.status_code == 200:
-        data = response.json()
+        data: dict = response.json()
         if data.get('_embedded'):
             return [item for item in data.get('_embedded', {}).get('items', [])]
         elif data.get('type'):
@@ -20,12 +21,13 @@ def fetch_files_info(public_key):
         return []
 
 
-def download_file(public_key):
+def download_file(public_key) -> str | None:
+    """Скачивает файл или папку по public_key."""
     url = f'https://cloud-api.yandex.net/v1/disk/public/resources/download?public_key={public_key}'
 
-    response = requests.get(url)
+    response: Response = requests.get(url)
     if response.status_code == 200:
-        data = response.json()
+        data: dict = response.json()
         link_for_download = data.get('href')
         return link_for_download
     else:
